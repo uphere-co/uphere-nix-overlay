6cb814bfa902f4e0aaf32f7b8740f2d155c2c114
Ian-Woo Kim <ianwookim@gmail.com>
2017-05-25 21:50:57 +0000

    inline-java packages are placed into uphere-nix-overlay

--- nix/haskell-modules/configuration-ghc-8.0.x.nix
+++ nix/haskell-modules/configuration-ghc-8.0.x.nix
@@ -3,6 +3,11 @@
 with pkgs;
 
 let
+    inline-java-src = fetchgit {
+      url = "git://github.com/wavewave/inline-java.git";
+      rev = "593cdf3a02a866c6822539c0e89adc8ed913a9ba";
+      sha256 = "1xngx5i7gpg4h33w6iznrphd1ji0f8dmf5lb5awsnxp72kszvqi5";
+    };
     llvmGeneralSrc = fetchgit {
       url = "git://github.com/wavewave/llvm-general.git";
       rev = "646bfa6168430b56035f9858c9676ac22fba4976";
@@ -199,5 +204,73 @@ in self: super: {
            description = "Yet Another YAML library";
            license = stdenv.lib.licenses.bsd3;
          }) {};
+
+      "inline-java" = self.callPackage
+        ({ mkDerivation, base, binary, bytestring, Cabal, containers
+         , directory, distributed-closure, filepath, ghc-heap-view, hspec
+         , inline-c, jni, jvm, language-java, process, singletons, stdenv
+         , syb, template-haskell, temporary, text, thread-local-storage
+         , vector
+         , jdk
+         }:
+         mkDerivation {
+           pname = "inline-java";
+           version = "0.6.2";
+           src = "${inline-java-src}";
+           libraryHaskellDepends = [
+             base binary bytestring Cabal containers directory
+             distributed-closure filepath ghc-heap-view inline-c jni jvm
+             language-java process singletons syb template-haskell temporary
+             text thread-local-storage vector
+           ];
+           testHaskellDepends = [
+             base bytestring hspec jni jvm singletons text
+           ];
+           buildDepends = [ jdk ];
+           homepage = "http://github.com/tweag/inline-java#readme";
+           description = "Java interop via inline Java code in Haskell modules";
+           license = stdenv.lib.licenses.bsd3;
+         }) { jdk = pkgs.jdk; };
+
+      "jvm" = self.callPackage
+        ({ mkDerivation, base, bytestring, distributed-closure, hspec, jni
+         , singletons, stdenv, text, vector
+         , jdk
+         }:
+         mkDerivation {
+           pname = "jvm";
+           version = "0.2.0";
+           src = "${inline-java-src}/jvm";
+           libraryHaskellDepends = [
+             base bytestring distributed-closure jni singletons text vector
+           ];
+           testHaskellDepends = [ base bytestring hspec text ];
+           buildDepends = [ jdk ];           
+           #doCheck = false;
+           homepage = "http://github.com/tweag/inline-java/tree/master/jvm#readme";
+           description = "Call JVM methods from Haskell";
+           license = stdenv.lib.licenses.bsd3;
+         }) { jdk = pkgs.jdk; };
+
+      "jni" = self.callPackage
+        ({ mkDerivation, base, bytestring, choice, containers, inline-c
+         , singletons, thread-local-storage
+         , cpphs, jdk
+         }:
+         mkDerivation {
+           pname = "jni";
+           src = "${inline-java-src}/jni";
+           version = "0.3.0";
+           libraryHaskellDepends = [
+             base bytestring choice containers inline-c singletons thread-local-storage
+           ];
+           setupHaskellDepends = [ cpphs ];
+           configureFlags = ["--extra-lib-dirs=${jdk.jre}/lib/openjdk/jre/lib/amd64/server"]; 
+           librarySystemDepends = [ jdk ];
+           homepage = "https://github.com/tweag/inline-java/tree/master/jni#readme";
+           description = "Complete JNI raw bindings";
+           license = stdenv.lib.licenses.bsd3;
+           hydraPlatforms = stdenv.lib.platforms.none;
+         }) {jdk = pkgs.jdk;};
 
     }
