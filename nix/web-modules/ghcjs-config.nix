0e31a4568cbb6ca2000151b23beb26b1cab0a8d1
Ian-Woo Kim <ianwookim@gmail.com>
2018-02-11 11:10:51 +0000

    fetchTarballFromGitHub

--- nix/web-modules/ghcjs-config.nix
+++ nix/web-modules/ghcjs-config.nix
@@ -4,10 +4,19 @@ self: super:
 
 with pkgs.haskell.lib;
 
-let removeLibraryHaskellDepends = pnames: depends:
+let
+    fetchTarballFromGitHub = { repo, owner, rev, sha256 }:
+      fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        inherit sha256;
+      };
+
+    removeLibraryHaskellDepends = pnames: depends:
        builtins.filter (e: !(builtins.elem (e.pname or "") pnames)) depends;
     ghcjsDom = import (pkgs.fetchFromGitHub { "owner" = "ghcjs"; "repo"= "ghcjs-dom"; "rev"= "b5984c8326f6eb1e29f2ca60c0e88eb6ecfdc0c1"; "sha256"= "1dspap1cgjz8fy98bxb18mxyi2gwscrcnzbfblki2jg11c4drll7"; }) self;
-    jsaddlePkgs = import (pkgs.fetchFromGitHub {   "owner"= "wavewave"; "repo"= "jsaddle"; "rev"= "b3f3e66ff3d1f37f6d070bdd986c01ca4db4f70d"; "sha256"=  "0fxrq0fa7c4z3m4rqaj38zi8a0v2zglqyh7b4jnidb9bb4gsk9x8"; }) self;
+    jsaddlePkgs = fetchTarballFromGitHub { "owner"= "wavewave"; "repo"= "jsaddle"; "rev"= "b3f3e66ff3d1f37f6d070bdd986c01ca4db4f70d"; "sha256"=  "0fxrq0fa7c4z3m4rqaj38zi8a0v2zglqyh7b4jnidb9bb4gsk9x8"; };
+
+      #import (pkgs.fetchFromGitHub {   "owner"= "wavewave"; "repo"= "jsaddle"; "rev"= "b3f3e66ff3d1f37f6d070bdd986c01ca4db4f70d"; "sha256"=  "0fxrq0fa7c4z3m4rqaj38zi8a0v2zglqyh7b4jnidb9bb4gsk9x8"; }) self;
 
 
 in
