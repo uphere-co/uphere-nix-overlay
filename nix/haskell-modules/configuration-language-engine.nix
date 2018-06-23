7abe75fd85e657f01b35c6895ba07819f0768a19
Ian-Woo Kim <ianwookim@gmail.com>
2018-06-23 22:25:25 +0000

    split configuration-language-engine.nix

---
+++ nix/haskell-modules/configuration-language-engine.nix
@@ -0,0 +1,69 @@
+{ pkgs
+, uphere-nix-overlay
+, event-analyzer
+, fetchfin
+, HCoreNLP
+, HUKB
+, network-transport-uphere
+, language-engine
+, nlp-pipeline
+, nlp-shared-types
+, compute-engine
+, textview
+, uphere-db
+, uphere-network-util
+, uphere-opaleye
+, corenlp
+, corenlp_models
+#
+, fetchgit
+, fetchurl
+, haskellPackages
+, stdenv
+, jdk
+, fasttext
+}:
+
+let
+
+  hsconfig1 = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.2.x.nix") { inherit pkgs; };
+  haskellPackages1 = pkgs.haskell.packages.ghc821.override { overrides = hsconfig1; };
+  fastTextNix = import (language-engine + "/fasttext/default.nix") {
+    inherit stdenv;
+    haskellPackages = haskellPackages1;
+  };
+  hsconfig2 = self: super: {
+    "event-analyzer"        = self.callPackage event-analyzer {};
+    "HCoreNLP"              = self.callPackage (import HCoreNLP) {
+                                inherit jdk corenlp corenlp_models;
+                              };
+    "HCoreNLP-Proto"        = self.callPackage (import (HCoreNLP + "/HCoreNLP-Proto")) {};
+    "network-transport-uphere" = self.callPackage (import network-transport-uphere) {};
+    "newsapi"               = self.callPackage (import (fetchfin + "/newsapi")) {};
+    "nlp-pipeline"          = self.callPackage (import nlp-pipeline) {};
+    "nlp-shared-types"      = self.callPackage (import nlp-shared-types) {};
+    "rss-scraper"           = self.callPackage (import (fetchfin + "/rss-scraper")) {};
+
+    "compute-engine"        = self.callPackage (import compute-engine) {};
+    "textview"              = self.callPackage (import textview) {};
+    "uphere-db"             = self.callPackage (import uphere-db) {};
+    "uphere-network-util" = self.callPackage (import uphere-network-util) {};
+    "uphere-opaleye"        = self.callPackage (import uphere-opaleye) {};
+
+    # LANGUAGE ENGINE for the time being
+    "fastText"              = self.callPackage fastTextNix { inherit fasttext; };
+    "HWordNet"              = self.callPackage (import (language-engine + "/HWordNet")) {};
+    "nlp-types"             = self.callPackage (import (language-engine + "/nlp-types")) {};
+  };
+  ukb = import (uphere-nix-overlay + "/nix/cpp-modules/ukb.nix") { inherit stdenv fetchgit fetchurl; boost = pkgs.boost; };
+
+  hsconfig3 = import (HUKB+ "/HUKB-driver/config.nix") { inherit uphere-nix-overlay ukb; };
+  hsconfig4 =
+    self: super: {
+      "HUKB-driver" = self.callPackage (import (HUKB + "/HUKB-driver")) {};
+    };
+in
+  self: super: (hsconfig1 self super //
+                hsconfig2 self super //
+                hsconfig3 self super //
+                hsconfig4 self super)
