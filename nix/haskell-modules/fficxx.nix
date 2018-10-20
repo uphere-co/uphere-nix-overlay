d1fa6ece74a105dca489af63662a8b934356cac9
Ian-Woo Kim <ianwookim@gmail.com>
2018-10-20 15:38:00 +0000

    update fficxx to 0.5

---
+++ nix/haskell-modules/fficxx.nix
@@ -0,0 +1,7 @@
+{ fetchgit }:
+
+fetchgit {
+  url = "https://github.com/wavewave/fficxx";
+  rev = "f6b00a53d03de024dbd1d18bdd94bb647ccbf465";
+  sha256 = "0wfgmax1ybf7j7b3s4xm707l8q4l53sbbfs02wrm0a1da25kgf00";
+}
