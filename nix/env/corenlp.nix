fb69c7070704765c856c9ba0b5a3590256f52ade
Ian-Woo Kim <ianwookim@gmail.com>
2018-10-20 22:31:42 +0000

    HCoreNLP as parameter instead of haskellPackages.

--- nix/env/corenlp.nix
+++ nix/env/corenlp.nix
@@ -1,9 +1,9 @@
 # This nix module provides an runtime environment for CoreNLP as setup hook.
 
-{ makeSetupHook, writeText, haskellPackages, corenlp, corenlp_models }:
+{ makeSetupHook, writeText, HCoreNLP, corenlp, corenlp_models }:
 
-# TODO: change hard-coded ghc version number.
+# TODO: change hard-coded corenlp/HCoreNLP/ghc version number.
 makeSetupHook { }
   (writeText "setup-hook.sh" ''
-     export CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${haskellPackages.HCoreNLP}/share/x86_64-linux-ghc-8.4.3/HCoreNLP-0.1.0.0/HCoreNLPProto.jar"
+     export CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar:${HCoreNLP}/share/x86_64-linux-ghc-8.4.3/HCoreNLP-0.1.0.0/HCoreNLPProto.jar"
   '')
